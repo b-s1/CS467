6b122a0188d1ce63c98891ff72380ab4d57fe39f
Jordan Bleck <jordan.bleck@gmail.com>
2019-07-26 15:09:54 -0700

    fixed server load script

--- BBB/startserver.sh
+++ BBB/startserver.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 
-if [ $# -lt 2 ] || [ $1 -lt 0 ] || [ $1 -gt 65535 ]
+
+if [ $# -lt 1 ] || [ $1 -lt 0 ] || [ $1 -gt 65535 ]
 then
    echo -e "\nrun with ./startserver.sh <port number> [&]\n"
    exit 0
 fi
 
+# source venv/bin/activate
 gunicorn --bind 0.0.0.0:$1 wsgi:app
